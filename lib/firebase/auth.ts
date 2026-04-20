23fa5116dd54d48da6a649082d80fa32343e130a
SayyadAdeel-a <adeelsayyad.a@gmail.com>
2026-04-20 11:40:36 +0500

    refactor: simplify auth module exports and wrap sendPasswordResetEmail to include auth instance

--- lib/firebase/auth.ts
+++ lib/firebase/auth.ts
@@ -1,11 +1,5 @@
-import { 
-  GoogleAuthProvider, 
-  signInWithPopup, 
-  signOut,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail
-} from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail as rawSendPasswordResetEmail } from "firebase/auth";
 import { auth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
@@ -29,8 +23,6 @@ export const logout = async () => {
   }
 };
 
-export { 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword, 
-  sendPasswordResetEmail 
-};
+export const createUserWithEmailAndPassword = createUserWithEmailAndPassword;
+export const signInWithEmailAndPassword = signInWithEmailAndPassword;
+export const sendPasswordResetEmail = (email: string) => rawSendPasswordResetEmail(auth, email);
