653e9546fdf172b021a219747b80142a7ba292be
SayyadAdeel-a <adeelsayyad.a@gmail.com>
2026-04-20 11:54:49 +0500

    refactor: alias firebase auth methods and wrap exports in utility functions

--- lib/firebase/auth.ts
+++ lib/firebase/auth.ts
@@ -1,5 +1,5 @@
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail as rawSendPasswordResetEmail } from "firebase/auth";
+import { createUserWithEmailAndPassword as rawCreateUserWithEmailAndPassword, signInWithEmailAndPassword as rawSignInWithEmailAndPassword, sendPasswordResetEmail as rawSendPasswordResetEmail } from "firebase/auth";
 import { auth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
@@ -23,6 +23,6 @@ export const logout = async () => {
   }
 };
 
-export const createUserWithEmailAndPassword = createUserWithEmailAndPassword;
-export const signInWithEmailAndPassword = signInWithEmailAndPassword;
+export const createUserWithEmailAndPassword = (email: string, password: string) => rawCreateUserWithEmailAndPassword(email, password);
+export const signInWithEmailAndPassword = (email: string, password: string) => rawSignInWithEmailAndPassword(email, password);
 export const sendPasswordResetEmail = (email: string) => rawSendPasswordResetEmail(auth, email);
